095432247619789e994cb49ca5b12a2dd4f695c8
Gabriel-Kasten <gabrieldegkasten@gmail.com>
2022-11-01 00:54:07 -0300

    fetch pokemon animated

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const API_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
 var imageBoard = document.getElementById("pokemon-board");
+var searchInput = document.getElementById("search-input");
 
+var terminalGif = document.getElementById("data-terminal");
 
-const pokemonFeth = () => {
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const pokemonFeth = async () => {
     var pokemon = document.getElementById("search-input").value;
     fetch(API_URL + pokemon)
         .then((response) => response.json())
         .then((data) => addPokemonImage(data));
+    terminalGif.src = "imgs/input.gif";
 }
 
 const addPokemonImage = (src) => {
-    imageBoard.src = src.sprites.front_default;
+    setTimeout(() => { terminalGif.src = "imgs/data_loaded.png"; imageBoard.src = src.sprites.front_default; }, 2600);
 }
